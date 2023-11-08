01a4fa16ddd45ff3c860ba5a57d321995b80f3f8
thaispaes <thaisreginapaesdossantos@gmail.com>
2023-11-07 22:38:54 -0300

    Feat: mostrando dados na tela

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pesquisa_cep/views/home_page.dart';
 
+
 void main() {
   HttpOverrides.global = MyHttpOverrides();
   runApp(MaterialApp(
@@ -18,7 +19,7 @@ void main() {
 class MyHttpOverrides extends HttpOverrides{
   @override
   HttpClient createHttpClient(SecurityContext? context){
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+      return super.createHttpClient(context)
+        ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pesquisa_cep/models/result_cep.dart';
 import 'package:pesquisa_cep/services/via_cep_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:easy_rich_text/easy_rich_text.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,6 +15,8 @@ class _HomePageState extends State<HomePage> {
   bool _loading = false;
   bool _enableField = true;
   String? _result;
+  late ResultCep _response = ResultCep();
+
 
   @override
   void dispose() {
@@ -32,7 +38,7 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             _buildSearchCepTextField(),
             _buildSearchCepButton(),
-            _buildResultForm()
+            _buildResultCep()
           ],
         ),
       ),
@@ -44,7 +50,12 @@ class _HomePageState extends State<HomePage> {
       autofocus: true,
       keyboardType: TextInputType.number,
       textInputAction: TextInputAction.done,
-      decoration: InputDecoration(labelText: 'Cep'),
+      decoration: const InputDecoration(
+          labelText: "Insira o CEP desejado",
+          labelStyle: TextStyle(color: Colors.grey),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+      ),
       controller: _searchCepController,
       enabled: _enableField,
     );
@@ -55,8 +66,51 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.only(top: 20.0),
       child: FloatingActionButton(
         onPressed: _searchCep,
+        backgroundColor: Colors.blue,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         child: _loading ? _circularLoading() : Text('Consultar'),
+
+      ),
+    );
+  }
+
+  Widget _buildResultCep() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, top: 30),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            RichText(
+                text: TextSpan(text: _response.cep ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.localidade ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.complemento ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.bairro ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.localidade ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.uf ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.ibge ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.unidade ?? '')
+            ),
+            RichText(
+                text: TextSpan(text: _response.gia ?? '')
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -73,7 +127,9 @@ class _HomePageState extends State<HomePage> {
     return Container(
       height: 15.0,
       width: 15.0,
-      child: CircularProgressIndicator(),
+      child: const CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+      ),
     );
   }
 
@@ -82,20 +138,21 @@ class _HomePageState extends State<HomePage> {
 
     final cep = _searchCepController.text;
 
-    final resultCep = await ViaCepService.fetchCep(cep: cep);
-    print(resultCep.localidade); // Exibindo somente a localidade no terminal
+    try {
+      final resultCep = await ViaCepService.fetchCep(cep: cep);
+      print(resultCep.localidade); // Exibindo somente a localidade no terminal
 
-    setState(() {
-      _result = resultCep.toJson();
-    });
+      setState(() {
+          _response = resultCep;
+      });
 
-    _searching(false);
-  }
-
-  Widget _buildResultForm() {
-    return Container(
-      padding: EdgeInsets.only(top: 20.0),
-      child: Text(_result ?? ''),
-    );
+      _searching(false);
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: 'Ocorreu um erro: $e',
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    }
   }
 }
